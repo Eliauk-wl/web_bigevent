bf91b8a8f4917ea397af6c543a771c4845fe3dd4
worry <2428405332@qq.com>
2020-09-12 11:00:20 +0800

    完成了首页功能的实现

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,5 +1,28 @@
-//每次在发起 请求时，都会先调用$.ajaxPrefilter()这个函数
+//每次在发起 任何 ajax请求时，都会先调用$.ajaxPrefilter()这个函数
 $.ajaxPrefilter(function(options) {
+    // 发起ajax时，统一拼接
     options.url = 'http://ajax.frontend.itheima.net' + options.url
-    console.log(options.url);
+        // console.log(options.url);
+
+
+    //统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+
+    // 全局挂载 complete函数
+    options.complete = function(res) {
+        // console.log('执行了complete函数');
+        // console.log(res);
+        // 在执行complete函数，直接拿到 res.responseJSON 从服务器响应回来的数值
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.清空内存 token
+            localStorage.removeItem('token')
+                // 2.强制返回登录页
+            location.href = '/login.html'
+        }
+    }
 })
